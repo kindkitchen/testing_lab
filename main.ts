c501ea954c9f138fd70aeda5c09c456e0fe18c02
Nik Deno <nik.deno@gmail.com>
2025-12-26 21:05:37 +0100

    first draft mvp for make_fixture

---
+++ main.ts
@@ -0,0 +1,74 @@
+const make_fixture = {
+  with_labels: <L extends string[]>() => {
+    const with_data_source_type = <D extends Record<string, unknown>>() => {
+      const with_state_derivations = <
+        P extends Record<
+          string,
+          (param: unknown) => (d: Partial<D>) => unknown
+        >,
+      >(
+        derivations_dict: P,
+      ) => {
+        const build = <T extends [Partial<D>, L[number][]][]>(
+          ...instances: T
+        ) => {
+          type tD = T[number][0];
+          type tL = T[number][1][number];
+          type tR = {
+            update_data_source: (
+              fn_logic: (actual: Partial<tD>) => Partial<tD>,
+            ) => void;
+            compute: {
+              [k in keyof P]: (
+                param: Parameters<P[k]>[0],
+              ) => Record<tL, () => ReturnType<ReturnType<P[k]>>>;
+            };
+          };
+
+          return instances.reduce((acc, instance) => {
+            const [_data_source, _labels] = instance;
+            const labels = [..._labels];
+            let data_source = structuredClone(_data_source) as Partial<tD>;
+
+            acc.update_data_source = (logic) => {
+              data_source = logic(data_source);
+            };
+
+            const compute = {} as any;
+
+            for (
+              const [state, derivation] of Object.entries(derivations_dict)
+            ) {
+              compute[state] = (param: any) => {
+                return labels.reduce((accL, label) => {
+                  if (!accL[label]) {
+                    accL[label] = () => {
+                      return derivation(param)(data_source as any) as any;
+                    };
+                  }
+                  return accL;
+                }, {} as Record<tL, () => ReturnType<ReturnType<P[keyof P]>>>);
+              };
+            }
+
+            acc.compute = compute;
+
+            return acc;
+          }, {} as tR);
+        };
+
+        return {
+          build,
+        };
+      };
+
+      return {
+        with_state_derivations,
+      };
+    };
+
+    return {
+      with_data_source_type,
+    };
+  },
+};
