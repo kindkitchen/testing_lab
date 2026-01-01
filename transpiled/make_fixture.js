ebb53d2ac6c48b4fb329eb88fd26fa66906afc26
Nik <nik.moiseienko@gmail.com>
2026-01-01 11:55:10 +0100

    attempt to make propper js export n1

--- transpiled/make_fixture.js
+++ transpiled/make_fixture.js
@@ -11,21 +11,82 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.make_fixture = void 0;
 exports.make_fixture = {
+    /**
+     * A lot of properties are syntax sugar with purpose
+     * to simplify process of constructing pretty difficult
+     * internal data-structure for fixture. Also it may
+     * help to understand main ideas and purposes with which
+     * it was developed.
+     */
     start_builder_chain: {
+        /**
+         * The shape of the data source
+         *
+         * This type is represent the all values,
+         * that can be used in dto, entities, models, etc.
+         * which will be produced from same source.
+         * Example:
+         * The single data-set associated with user can be used
+         * for user creation, user updates, view full or compact
+         * user's representation, etc.
+         * ```
+         * make_fixture.for_data_type<{
+         *   id: string;
+         *   name: string;
+         *   age: number;
+         * }>() /// ...rest code
+         * ```
+         * #### Important!
+         * **This is generic type, that you should provide and nothing more**
+         */
         for_data_type: function () {
             var _warning = [];
             for (var _i = 0; _i < arguments.length; _i++) {
                 _warning[_i] = arguments[_i];
             }
             return {
+                /**
+                 * Next generic only helper, that should extend
+                 * `string` type, for example:
+                 * ```
+                 * .with_possible_tags<"all", "verified_only", "men">
+                 * ```
+                 * These tags will be used to mark fixtures and so have ability
+                 * to group them by some criteria. Because this is tag - fixture
+                 * can be belong to many groups at once.
+                 *
+                 * **At this moment though - you should simply register all possible variants
+                 * for better typescript inference**
+                 */
                 with_possible_tags: (function () {
                     var _warning = [];
                     for (var _i = 0; _i < arguments.length; _i++) {
                         _warning[_i] = arguments[_i];
                     }
                     return {
+                        /**
+                         * This is object, in which each property is represented
+                         * particular dto/entity/view or state of your data.
+                         * For example data, associated with user can contain many
+                         * properties, but during creation you skip `id` and so on.
+                         * The value of each property is a function - that should
+                         * transform initial data to some form.
+                         * Example:
+                         * ```
+                         * {
+                         *    create_user_dto: (data) => ({ : data.email }),
+                         *    update_user_dto: (data) => ([data.id, ])
+                         * }
+                         * ```
+                         */
                         data_can_be_transformed_into_such_views: function (transformer) {
                             return {
+                                /**
+                                 * Complete building fixture-set by providing
+                                 * implementation.
+                                 * Each property is fixture-wrapper, with fixture itself
+                                 * and all tags associated with this fixture.
+                                 */
                                 build: function (fixture_set) {
                                     var last_id = Date.now();
                                     var db = Object
@@ -57,7 +118,7 @@ exports.make_fixture = {
                                                     tags[_i] = arguments[_i];
                                                 }
                                                 return tags.forEach(function (tag) {
-                                                    var fixture = db.name_fixture.get(name);
+                                                    var fixture = db.name_fixture.get(name); /// 100% exists because all fixtures are provided at once;
                                                     (db.name_tag_fixture.get(name) ||
                                                         db.name_tag_fixture.set(name, new Map())
                                                             .get(name))
