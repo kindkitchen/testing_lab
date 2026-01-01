af65cad940e18e5ae7f60f6e7d4f18bbde27728b
Nik <nik.moiseienko@gmail.com>
2026-01-01 23:36:39 +0100

    use tsup instead of tsc to generate cjs version

---
+++ .config/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+import * as path from "@std/path";
+
+const cwd = Deno.cwd();
+
+export default defineConfig({
+  entry: {
+    make_fixture: path.join(cwd, "src", "make_fixture.ts"),
+  },
+  format: "cjs",
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  cjsInterop: true,
+  outDir: path.join(cwd, "transpiled"),
+});
